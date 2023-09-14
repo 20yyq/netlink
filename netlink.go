e21455c46986091536cdba88b2bc691e023616d3
Eacher <20yyq@163.com>
2023-09-14 10:52:21 +0800

    修复自定义接收方式。没有复位读超时行为

--- netlink.go
+++ netlink.go
@@ -1,7 +1,7 @@
 // @@
 // @ Author       : Eacher
 // @ Date         : 2023-09-11 11:04:00
-// @ LastEditTime : 2023-09-14 10:31:08
+// @ LastEditTime : 2023-09-14 10:50:26
 // @ LastEditors  : Eacher
 // @ --------------------------------------------------------------------------------<
 // @ Description  : 
@@ -53,13 +53,16 @@ func (nlr *NetlinkRoute) Exchange(sm *SendNLMessage, rm *ReceiveNLMessage) error
 	nlr.isExchange, sm.sa = true, syscall.Sockaddr(nlr.Sal)
 	if err := nlr.write(sm.sendto); sm.Err == nil {
 		if sm.Err = err; sm.Err == nil {
-			f := rm.recvfrom
+			f, custom := rm.recvfrom, false
 			if rm.Exchange != nil && rm.OutTime > 0 {
 				nlr.f.SetReadDeadline(time.Now().Add(rm.OutTime))
-				f = rm.Exchange
+				f, custom = rm.Exchange, true
 			}
 			if err = nlr.read(f); rm.Err == nil {
 				rm.Err = err
+			}
+			if custom {
+				nlr.f.SetReadDeadline(0) 
 			}
 			return rm.Err
 		}
